84a92e60c9d2ca67d7cee2fafcc7c38f9d5f6798
xypxyp470 <xypxyp470@163.com>
2020-05-27 08:53:58 +0800

    注释

---
+++ src/main/java/com/dcits/springmvc/service/UserService.java
@@ -0,0 +1,13 @@
+package com.dcits.springmvc.service;
+
+import com.dcits.springmvc.vo.UserVO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public UserVO get(Integer id) {
+        return new UserVO().setId(id).setUsername("test");
+    }
+
+}
